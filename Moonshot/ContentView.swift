03c49e0f5d482321b55907841290d59c6e25b6ba
btabaska <btabaska@gmail.com>
2020-06-22 14:58:50 -0700

    Initial Commit

---
+++ Moonshot/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Moonshot
+//
+//  Created by Brandon Tabaska on 6/22/20.
+//  Copyright © 2020 Brandon Tabaska. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
